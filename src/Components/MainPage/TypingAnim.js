641863852bd2463b8629c262ec6fd433aa21cf75
piro56 <meetvk57@gmail.com>
2023-01-05 14:38:55 -0500

    ayooo

---
+++ src/Components/MainPage/TypingAnim.js
@@ -0,0 +1,27 @@
+import { TypeAnimation } from 'react-type-animation';
+export default function TypingAnim() {
+    var currentTime = new Date();
+    var month = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(currentTime);
+    var day = currentTime.getDay().toString();
+    var year = currentTime.getFullYear().toString()
+    var str = `Today is ${month} ${day}, ${year}`;
+    console.log(str);
+
+    return (
+        <TypeAnimation
+            sequence={[
+            'Welcome!', // Types 'One'
+            2000, // Waits 1s
+            str,
+            2000,
+            () => {
+                // CALLBACK
+            }
+            ]}
+            wrapper="div"
+            cursor={true}
+            repeat={Infinity}
+            style={{ fontSize: '2em' }}
+        />
+        );
+}
