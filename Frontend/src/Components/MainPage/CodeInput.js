cab789baa780bd51cd426722c81771b01cdb65d5
piro56 <69094266+piro56@users.noreply.github.com>
2023-05-20 17:08:06 -0500

    Some backend stuff

--- Frontend/src/Components/MainPage/CodeInput.js
+++ Frontend/src/Components/MainPage/CodeInput.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
+const BACKEND_SERVER = "http://localhost:8080";
+
 
 function inputBlur(e, setForm, defaultForm, setButton) {
-    if (e.target.value=="") { setForm(defaultForm); setButton(false);}
+    if (e.target.value === "") { setForm(defaultForm); setButton(false);}
     else {
         setButton(true);
     }
@@ -10,15 +12,20 @@ function inputBlur(e, setForm, defaultForm, setButton) {
 
 function inputChange(e, setForm, defaultForm, setButton) {
     setForm(e.target.value);
-    if (e.target.value != "" && e.target.value != defaultForm) {
+    if (e.target.value !== "" && e.target.value !== defaultForm) {
         setButton(true);
     } else (setButton(false));
 }
 
-function SecretSubmit(e, form, navigate) {
-    if (form == "Secret") {
-        navigate("/resume")
-    }
+function SecretSubmit(e, form, navigate) { 
+
+    fetch(BACKEND_SERVER + "/secret", {
+        method: 'GET',
+        headers: new Headers({'Authorization': form})
+    }).then((res) => res.text()).then((html) => {
+        navigate("/secret")
+        // document.body.innerHTML = html;
+    });
 }
 
 export default function CodeInput() {
@@ -32,12 +39,12 @@ export default function CodeInput() {
     return (
         <div className="App-SecretInput">
             <input type="text" 
-            style={(form == defaultForm) ? faded : white}
+            style={(form === defaultForm) ? faded : white}
             className="CI-SecretInput"
             defaultValue={"Secret Code"}
             onChange={e => inputChange(e, setForm, defaultForm, setButton)}
             onBlur={e => inputBlur(e, setForm, defaultForm, setButton)}
-            onFocus={e => {if (e.target.value == defaultForm) {setForm("");}}}
+            onFocus={e => {if (e.target.value === defaultForm) {setForm("");}}}
             value={form}
             />
             {showButton ? <button className="CI-SecretButton" onClick={e => SecretSubmit(e, form, navigate)}>Submit</button> : null}
