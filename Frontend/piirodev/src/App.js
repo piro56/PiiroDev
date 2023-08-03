41413a248c4bc42bbbb84ad8dd520f1b42519b3e
piro56 <69094266+piro56@users.noreply.github.com>
2023-08-02 22:41:16 -0500

    background effects

--- Frontend/piirodev/src/App.js
+++ Frontend/piirodev/src/App.js
@@ -1,15 +1,18 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Navigation/navbar';
 import FlagCarousel from './Components/flagcarousel';
+import BackgroundParticles from './Components/particles';
 import './scss/app.scss';
+import './Components/background.scss';
 
 function App() {
   return (
-    <div>
+    <div style={{height: '100%', position: 'relative'}}>
+      <BackgroundParticles/>
       <NavBar />
+      <h1 class="text-center">Still Under Construction</h1>
+
       <FlagCarousel />
-
-
 
 
     </div>
---
+++ Frontend/piirodev/src/Components/particles.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './background.scss';
+
+
+export default function BackgroundParticles() {
+    return (
+        <div class="particle-container w-100 h-100">
+            <div class="particle particle-1"></div>
+            <div class="particle particle-2"></div>
+            <div class="particle particle-3"></div>
+            <div class="particle particle-4"></div>
+        </div>
+    );
+}
