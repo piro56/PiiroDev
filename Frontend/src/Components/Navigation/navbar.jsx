694b0ae93191390b7270159ca13ccbbd9b224e6e
piro56 <69094266+piro56@users.noreply.github.com>
2023-10-22 02:19:06 -0400

    icon

--- Frontend/src/Components/Navigation/navbar.jsx
+++ Frontend/src/Components/Navigation/navbar.jsx
@@ -68,14 +68,14 @@ export default function NavBar() {
                 </h1>
                 <div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <h1>&#9776;</h1>
                 </button>
-                <nav>
+                <nav className="navbar navbar-dark">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                             <a className="nav-link" href="#t" >About Me</a>
-                             <a className="nav-link" href="https://notes.piiro.dev/" >Writing</a>
+                             <a className="nav-link text-secondary" href="#t" >About Me</a>
+                             <a className="nav-link text-secondary" href="https://notes.piiro.dev/" >Writing</a>
                             </li>
                         </ul>
                     </div>
