6cf21543b880ba8f2614ae7a27a8bded3462cfea
piro56 <69094266+piro56@users.noreply.github.com>
2023-10-22 02:50:59 -0400

    cleanup

--- Frontend/src/Components/Navigation/navbar.jsx
+++ Frontend/src/Components/Navigation/navbar.jsx
@@ -21,12 +21,11 @@ export default function NavBar() {
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-dark bg-body justify-content-center p-0" style={{ marginTop: '20px' }}>
                 <div className="container">
-                    <div className="row w-100 h-100 justify-content-center align-items-center text-center">
-
-                        <div className="col-4 nav-section d-grid flex-column h-100">
+                    <div className="row w-100 h-100 justify-content-center align-items-end text-center">
+                        <div className="h-100 col-4 nav-section d-inline">
                             <div className="row h-100">
-                                <div className="col-12 nav-item d-flex justify-content-center align-items-center">
-                                    <a className="nav-link nav-sub text-med" href="#t" >About Me</a>
+                                <div className="col-12 nav-item d-inline-block justify-content-center align-items-bottom h-100">
+                                    <a className="nav-link nav-sub" href="me" >About Me</a>
                                 </div>
                             </div>
                         </div>
@@ -41,13 +40,13 @@ export default function NavBar() {
                         <div className="col-4 nav-section">
                             <div className="row">
                                 <div className="col-6 nav-item align-middle">
-                                    <a className="nav-link nav-sub text-med" href="https://notes.piiro.dev/" >Writing</a>
+                                    <a className="nav-link nav-sub" href="https://notes.piiro.dev/" >Writing</a>
                                 </div>
                                 <div className="col-6 nav-item dropdown">
                                     <a className="nav-link nav-sub text-med dropdown-toggle" href="#dropdown"
                                         id="navbarDropdown" role="button" data-bs-toggle="dropdown"
                                         aria-expanded="false">Info</a>
-                                    <div className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+                                    <div className="dropdown-menu bg-primary nav-sub" aria-labelledby="navbarDropdown">
                                         <a className="dropdown-item" href="#1">not</a>
                                         <a className="dropdown-item" href="#2">done</a>
                                         <a className="dropdown-item" href="#3">yet</a>
@@ -74,7 +73,7 @@ export default function NavBar() {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                             <a className="nav-link text-secondary" href="#t" >About Me</a>
+                             <a className="nav-link text-secondary" href="me" >About Me</a>
                              <a className="nav-link text-secondary" href="https://notes.piiro.dev/" >Writing</a>
                             </li>
                         </ul>
