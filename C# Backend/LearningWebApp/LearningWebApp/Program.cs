baaebaccf096d8d1cc05b551cff0dd3d91ce494c
piro56 <meetvk57@gmail.com>
2023-10-30 22:50:54 -0400

    ignore

--- C# Backend/LearningWebApp/LearningWebApp/Program.cs
+++ C# Backend/LearningWebApp/LearningWebApp/Program.cs
@@ -1,3 +1,5 @@
+using LearningWebApp.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+MongoDBConn dbConn = MongoDBConn.Instance;
+dbConn.init();
 
 app.Run();
---
+++ C# Backend/LearningWebApp/LearningWebApp/Utils/CredentialsReader.cs
@@ -0,0 +1,20 @@
+﻿namespace LearningWebApp.Utils
+{
+    public class CredentialsReader
+    {
+
+        // String[] in form [username, password]
+        public static string[] getMongoDBCredentials(string file)
+        {
+
+            string[] text = File.ReadAllLines(file);
+            if (text.Length != 2)
+            {
+                return null;
+            }
+            return text;
+
+        }
+
+    }
+}
